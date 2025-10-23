027452fef4189f1d9df837c66f0ee7dabd730bd0
vybdani <darochap@uamv.edu.ni>
2025-10-22 23:17:40 -0600

    Agregar archivos de proyecto.

---
+++ Aplicacion Windows Form/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Aplicacion_Windows_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Saludo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(77, 91);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 22);
+            this.tbNombre.TabIndex = 1;
+            this.tbNombre.TextChanged += new System.EventHandler(this.tbNombre_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.AutoSize = true;
+            this.txtNombre.Location = new System.Drawing.Point(74, 51);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(117, 16);
+            this.txtNombre.TabIndex = 2;
+            this.txtNombre.Text = "Ingresa tu nombre:";
+            this.txtNombre.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 263);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Label txtNombre;
+    }
+}
+
---
+++ Aplicacion Windows Form/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicacion_Windows_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Si el usuario escribe su nombre en textBox1 y luego presiona button1, ¿cómo usarías el código MessageBox.Show(...) para que el mensaje muestre "Hola" seguido del nombre que está escrito en el textBox1?
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void tbNombre_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Nombre = tbNombre.Text;
+            MessageBox.Show("Holiss " + Nombre);
+        }
+    }
+}
